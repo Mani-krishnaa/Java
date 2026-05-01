e9c9801d17179986e48636e6bd2cdc9e6e1de4b1
Mani Krishna <manikrishna4849@gmail.com>
2026-05-01 12:24:31 +0530

    add initial implementation of Array class with main method

---
+++ arrays/ArrayBasics.java
@@ -0,0 +1,13 @@
+package arrays;
+
+class Array {
+
+    public static void main(String a[]) {
+        int[] arr = { 1, 2, 3, 4 };
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[i]);
+
+        }
+    }
+}
