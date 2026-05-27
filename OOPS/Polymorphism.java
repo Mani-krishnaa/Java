f578798a3d6f48c07f5589e99a639c6124d217d6
Mani Krishna <manikrishna4849@gmail.com>
2026-05-27 09:47:03 +0530

    add implementation of AcessModifiers and Polymorphism classes demonstrating runtime polymorphism

---
+++ OOPS/Polymorphism.java
@@ -0,0 +1,38 @@
+package OOPS;
+
+//Run time polymorphism
+
+class A {
+    public void show() {
+        System.out.println("In A Show");
+    }
+}
+
+class B extends A {
+    public void show() {
+        System.out.println("In B Show");
+    }
+}
+
+class C extends A {
+    public void show() {
+        System.out.println("In C Show");
+    }
+}
+// class C extends A {
+
+// }
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+
+        obj = new B();
+        obj.show();
+
+        obj = new C();
+        obj.show();
+    }
+
+}
